ecdd7021ecafc12989fbb071d7766a93322449d4
Богдан <1k.bhdn@gmail.com>
2021-10-16 19:23:10 +0300

    Прикрутил документ фрагмент

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@ const ADS_COUNT = 10;
 const data = [];
 const mapCanvas = document.querySelector('.map__canvas');
 const offerTemplate = document.querySelector('#card').content.querySelector('.popup');
+const documentFragment = document.createDocumentFragment();
 
 for (let i = 0; i < ADS_COUNT; i++) {
   data.push(generateAdData(i + 1));
   const offer = createAd(data[i], offerTemplate);
 
-  mapCanvas.appendChild(offer);
+  documentFragment.appendChild(offer);
+  mapCanvas.appendChild(documentFragment);
 }
