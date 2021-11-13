45379176762db134b2e77b5fe25f056068d4688a
Богдан <1k.bhdn@gmail.com>
2021-11-13 16:44:11 +0200

    12.10. Фото на память (необязательное задание)

--- js/ad-form.js
+++ js/ad-form.js
@@ -1,9 +1,66 @@
 import {disableForm} from './util.js';
 import {adForm, adFormPrice} from './ad-form-validate.js';
 
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
 const adFormCoords = document.querySelector('#address');
 const successTemplate = document.querySelector('#success').content.querySelector('.success');
 const errorTemplate = document.querySelector('#error').content.querySelector('.error');
+const avatarChooser = adForm.querySelector('input[name=avatar]');
+const avatarPreview = adForm.querySelector('.ad-form-header__preview img');
+const imagesContainer = adForm.querySelector('.ad-form__photo-container');
+const imagesChooser = adForm.querySelector('input[name=images]');
+const imagesPreviewContainer = adForm.querySelector('.ad-form__photo');
+
+avatarChooser.addEventListener('change', () => {
+  const file = avatarChooser.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+  if (matches) {
+    avatarPreview.src = URL.createObjectURL(file);
+  }
+});
+
+const validateImg = (files, index) => {
+  let img;
+  let newContainer;
+
+  const fileName = files[index].name.toLowerCase();
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+  if (matches) {
+    if (!imagesPreviewContainer.querySelector('img')) {
+      img = document.createElement('img');
+      img.width = 70;
+      img.height = 70;
+    } else {
+      newContainer = imagesPreviewContainer.cloneNode(true);
+      img = newContainer.querySelector('img');
+    }
+
+    img.src = URL.createObjectURL(files[index]);
+
+    if (newContainer) {
+      imagesContainer.appendChild(newContainer);
+    } else {
+      imagesPreviewContainer.appendChild(img);
+    }
+  }
+};
+
+imagesChooser.addEventListener('change', () => {
+  const images = imagesChooser.files;
+
+  if (images.length > 1) {
+    for (let i = 0; i < images.length; i++) {
+      validateImg(images, i);
+    }
+  } else {
+    validateImg(images, 0);
+  }
+});
 
 const resetAdFrom = () => {
   adForm.reset();
